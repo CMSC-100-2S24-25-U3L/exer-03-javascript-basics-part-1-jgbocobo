49cae0c70a4d81208563561247736944376df9d5
jgbocobo <jgbocobo@up.edu.ph>
2025-02-19 11:46:18 +0800

    Added reverse function

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function validatePassword(x,y){
         return false;
     }
 }
+
+// Function to reverse the string
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
